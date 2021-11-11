392a277b3a723884ccd8b057786a858fe2dfb602
Steffen Kroggel <developer@steffenkroggel.de>
2021-11-11 13:26:51 +0100

    [UPDATE] update dependency rkw_mailer

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,12 +21,12 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '8.7.57',
+    'version' => '8.7.58',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-7.6.99',
 			'rkw_basics' => '8.7.0-8.7.99',
-			'rkw_mailer' => '8.7.0-8.7.99',
+			'rkw_mailer' => '8.7.0-9.5.99',
 			'rkw_registration' => '8.7.0-8.7.99',
             'rte_ckeditor' => '8.7.31-8.7.99',
         ],
